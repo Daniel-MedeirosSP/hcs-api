f8ab87ade8fbc82c646eb7f6ee3dccd24a46aa62
Daniel Medeiros <daniel.medeirossp@gmail.com>
2025-07-19 19:55:43 -0300

    primeiro input, com a base do projeto

---
+++ src/main/java/com/medeiros/hcs/HcsApplication.java
@@ -0,0 +1,13 @@
+package com.medeiros.hcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medeiros/hcs/HcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medeiros.hcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
